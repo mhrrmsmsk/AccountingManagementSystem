926a613dfb2bcde91a700cf0b272639ac7c271f3
mhrrmsmsk <smuharrem121@gmail.com>
2025-11-13 01:37:14 +0300

    migration

---
+++ AccountSystem/Migrations/20251112155610_UpdateProductColumns.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        IF NOT EXISTS (
+            SELECT * 
+            FROM INFORMATION_SCHEMA.COLUMNS 
+            WHERE TABLE_NAME = 'products' 
+              AND COLUMN_NAME = 'CurrentStock'
+        )
+        BEGIN
+            ALTER TABLE products
+            ADD CurrentStock int NOT NULL DEFAULT 0;
+        END
+    ");
+
+            migrationBuilder.CreateTable(
+                name: "ProductDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    ProductCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UnitName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Barcode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PurchasePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SalePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    VatRate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MinStock = table.Column<int>(type: "int", nullable: false),
+                    MaxStock = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductDetails");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentStock",
+                table: "products");
+        }
+    }
+}
